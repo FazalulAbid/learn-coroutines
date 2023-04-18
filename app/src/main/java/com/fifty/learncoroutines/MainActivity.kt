2edc6a99229b4a80a43bb5ea628e3df9f7835568
Fazalul Abid <fazalulabid007@gmail.com>
2023-04-18 14:19:36 +0530

    Coroutine Global Scope and delay.

--- app/src/main/java/com/fifty/learncoroutines/MainActivity.kt
+++ app/src/main/java/com/fifty/learncoroutines/MainActivity.kt
@@ -2,16 +2,27 @@ package com.fifty.learncoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        something()
+
+        // Every coroutine does need a coroutine scope to work.
+        // Global scope means, this coroutine will live as long the application does.
+        // But of course it will die if it finishes its job.
+        GlobalScope.launch {
+            delay(3000L)
+            Log.d(TAG, "Coroutine says hello from thread ${Thread.currentThread().name}")
+        }
+        Log.d(TAG, "Hello from thread ${Thread.currentThread().name}")
     }
 
     private fun something() = runBlocking {
